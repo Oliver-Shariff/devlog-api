b775247f02e58f979b98328f5dd5742d389ac91e
Oliver-Shariff <143436031+Oliver-Shariff@users.noreply.github.com>
2025-11-17 08:48:40 -0500

    Summarize feature working
    -Using OpenAI api, entry texts can be summarized asynchronously

--- app/ai.py
+++ app/ai.py
@@ -1,4 +1,35 @@
 from sqlalchemy.orm import Session
 from sqlalchemy.exc import IntegrityError
-from sqlalchemy import update, select
-from app.models import Entry, Tag
+from app.models import Entry
+from openai import OpenAI
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+OPENAI_SUMMARIZE_KEY = os.getenv("OPENAI_SUMMARIZE_KEY")
+
+client = OpenAI(
+    api_key=OPENAI_SUMMARIZE_KEY
+)
+
+def summarize_entry_text(entry_id: int, user_email, db: Session ) -> str:
+    entry = db.get(Entry, entry_id)
+
+    if not entry or entry.user_email != user_email:
+        return None
+    
+    entry_text = entry.content
+    response = client.chat.completions.create(
+        model="gpt-4o-mini",
+        messages=[
+            {"role": "system", "content": "You are an assistant that summarizes user journal entries into concise reflections."},
+            {"role":"user", "content": f"Summarize this entry in 2-3 scentences:\n\n{entry_text}"}
+        ],
+        temperature=0.7,
+        max_tokens=150,
+    )
+
+    summary = response.choices[0].message.content.strip()
+    return summary
+
--- app/routers/ai.py
+++ app/routers/ai.py
@@ -2,9 +2,16 @@ from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from sqlalchemy.exc import IntegrityError
 from app.database import get_db
-from app.crud import entries as entries_crud
-from app.schemas import EntryCreate, EntryUpdate, EntryRead
+import app.ai as ai_functions
 import app.security as security
 from app.models import User
 
 router = APIRouter(prefix="/api/ai", tags=["ai"])
+
+@router.post("/summarize/{entry_id}")
+async def summarize_entry(entry_id: int, db: Session= Depends(get_db), current_user: User = Depends(security.get_current_user)):
+    summary = ai_functions.summarize_entry_text(entry_id=entry_id, user_email=current_user.email, db=db)
+
+    if not summary:
+        raise HTTPException(status_code=404, detail = "Entry does not exist or does not belong to you.")
+    return summary
